f24bfcc23b54412d85b7235fdfaab8e680f1ab92
GulKaratas <glsrn_7872@outlook.com>
2026-02-21 23:36:11 +0300

    Add project files.

---
+++ TravelsalCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelsalCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
